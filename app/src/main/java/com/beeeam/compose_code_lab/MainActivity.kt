8f908ee729227033d8f582b26353c856826066fc
BEEEAM-J <cbj0545@naver.com>
2023-12-01 02:27:29 +0900

    Until STEP 6

--- app/src/main/java/com/beeeam/compose_code_lab/MainActivity.kt
+++ app/src/main/java/com/beeeam/compose_code_lab/MainActivity.kt
@@ -1,28 +1,34 @@
-package com.beeeam.compose_code_lab
+package com.beeeam.compose_code_lab // ktlint-disable package-name
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.beeeam.compose_code_lab.ui.theme.Compose_code_labTheme
+import androidx.compose.ui.unit.dp
+import com.beeeam.compose_code_lab.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Compose_code_labTheme {
+            BasicsCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+                    Greeting("BEEEAM")
                 }
             }
         }
@@ -30,17 +36,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    val name = listOf("BEEEAM", "JUN")
+    Surface(modifier = Modifier.padding(vertical = 4.dp)) {
+        Column {
+            for (text in name) {
+                Greeting(text)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greeting(text: String) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+    ) {
+        Row(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
+            Column(Modifier.weight(1f)) {
+                Text(text = "Hello")
+                Text(text = text)
+            }
+            ElevatedButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Text(text = "Show more")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
-    Compose_code_labTheme {
-        Greeting("Android")
+    BasicsCodelabTheme {
+        MyApp()
     }
 }
